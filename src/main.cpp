cbfa1868bcbf2794f561f938dab05ecc3826ce6c
Kelly Brener-Raffalli <kbrener-@c3r2p6.42perpignan.fr>
2025-01-17 11:53:36 +0100

    lunch_break_have_to_applicate_request_class

--- src/main.cpp
+++ src/main.cpp
@@ -5,5 +5,18 @@
 4. Create servers -> run
 5. Loop
     5.1 Check clients ()
-    5.2 
+    5.2
 */
+
+#include "../include/ConfigParser.hpp"
+#include "../include/Server.hpp"
+
+int	main(int argc, char **argv) {
+	if (argc > 2)
+		return 1;
+	std::string	configFile;
+	if (argc == 2)
+		configFile = argv[1];
+	ConfigParser	config(configFile);
+	Server	server(config.getServers())
+}
