409879158e418c1a0769e3f3fb6420a534b374e1
darambae <64197421+jazzy1101@users.noreply.github.com>
2025-01-18 05:11:59 +0100

    saving before merging

--- src/configParser.cpp
+++ src/configParser.cpp
@@ -13,10 +13,7 @@ save the data in a struct and return a list of servers info.*/
 */
 
 ConfigParser::ConfigParser(const std::string& file) {
-    if (file.empty())
-        this->setFilePath("default.conf");
-    else
-        this->setFilePath(file);
+    this->setFilePath(file);
 }
 
 void    ConfigParser::setFilePath(const std::string& file) {
@@ -30,31 +27,29 @@ void    ConfigParser::setServers(const ConfigServer& server) {
     this->servers.push_back(server);
 }
 
-bool ConfigParser::parseFile() {
+void    ConfigParser::parseFile() {
     ConfigServer server;
     std::string line;
     std::ifstream file(this->getFilePath());
 
     if (!file.is_open())
-        throw "Unable to open the file.";
+        throw "Configuration file cannot be opened.";
 
     while (getline(file, line)) {
         if (line.empty() || line[0] == '#') { continue; }
         if (line.find("server {") != std::string::npos) {
-            this->parseDirectives(file, server);
+            parseDirectives(file, server);
             setServers(server);
         }
     }
     if (file.is_open())
         file.close();
-    return true;
 }
 
-
-bool    ConfigParser::parseDirectives(std::ifstream &file, ConfigServer &server) {
+void    ConfigParser::parseDirectives(std::ifstream &file, ConfigServer &server) {
     std::string line;
     while (getline(file, line)) {
-        if (line.find("}") != std::string::npos) { return true; }
+        if (line.find("}") != std::string::npos) { break; }
         if (line.empty() || line[0] == '#') { continue; }
         if (line.find("listen ") != std::string::npos && line.find(";") != std::string::npos) {
             if (line.find(":") != std::string::npos) {
@@ -84,17 +79,16 @@ bool    ConfigParser::parseDirectives(std::ifstream &file, ConfigServer &server)
             parseLocation(file, line, location);
             server.setLocations(location);
         }
-        return false;
     }
 }
 
-bool ConfigParser::parseLocation(std::ifstream &file, std::string line, ConfigLocation &location) {
+void    ConfigParser::parseLocation(std::ifstream &file, std::string line, ConfigLocation &location) {
     size_t start_pos = line.find(" ") + 1;
     size_t end_pos = line.find(";");
     std::string path = line.substr(start_pos, line.find("{") - start_pos);
     location.setPath(path);
     while (getline(file, line)) {
-        if (line.find("}") != std::string::npos) { return true; }
+        if (line.find("}") != std::string::npos) { break; }
         if (line.empty() || line[0] == '#') { continue; }
         if (line.find("root ") != std::string::npos)
             location.setRoot(line.substr(line.find(" ") + 1, end_pos - start_pos));
@@ -108,11 +102,14 @@ bool ConfigParser::parseLocation(std::ifstream &file, std::string line, ConfigLo
             location.setReturn(line.substr(line.find(" ") + 1, line.find(";")));
         
     }
-    return false;
 }
 
 bool    ConfigParser::validMethods(const std::set<std::string>& methods) {
-    std::set<std::string> validMethods = {"GET", "POST", "DELETE", "PUT"};
+    std::set<std::string> validMethods;
+    validMethods.insert("GET");
+    validMethods.insert("POST");
+    validMethods.insert("DELETE");
+    validMethods.insert("PUT");
     for (std::set<std::string>::iterator it = methods.begin(); it != methods.end(); ++it) {
         if (validMethods.find(*it) == validMethods.end())
             return false;
@@ -129,19 +126,19 @@ bool    ConfigParser::validIp(std::string ip) {
         return true;
     while ((pos = ip.find(".")) != std::string::npos) {
         segment = ip.substr(0, pos);
-        num = stoi(segment);
+        num = std::atoi(segment.c_str());
         if (num < 0 || num > 255)
             return false;
         ip = ip.substr(pos + 1);
     }
-    num = stoi(ip);
+    num = std::atoi(ip.c_str());
     if (num < 0 || num > 255)
         return false;
     return true;
 }
 
 bool    ConfigParser::validPort(const std::string& port) {
-    int num = std::stoi(port);
+    int num = std::atoi(port.c_str());
     if (num < 0 || num > 65535)
         return false;
     return true;
@@ -154,37 +151,38 @@ bool    ConfigParser::validAutoindex(const std::string& line) {
 }
 
 bool    ConfigParser::validErrorPage(const std::string& line) {
-    std::list<std::string> tmp_list = splitString<std::list<std::string>>(line, ' ');
-    while (!tmp_list.empty()) {
+    std::cout << line << std::endl;
+    std::list<std::string> tmp_list = splitString<std::list<std::string> >(line, ' ');
+    while (tmp_list.size() > 0) {
         const std::string& token = tmp_list.front();
-        if (token[0] == '/') {
-            if (tmp_list.size() > 1)
-                return false;
+        if (onlyDigits(token) && std::atoi(token.c_str()) >= 300 && std::atoi(token.c_str()) <= 599) {
+            tmp_list.pop_front();
+            continue;
+        }
+        if (token[0] == '/' && tmp_list.size() == 1) {
+            tmp_list.pop_front();
             return true;
         }
-        if (!onlyDigits(token)|| stoi(token) < 300 || stoi(token) > 599)
-            return false;
-        tmp_list.pop_front();
     }
     return false;
 }
 
 bool    ConfigParser::validReturn(const std::string& line) {
-    std::list<std::string> tmp_list = splitString<std::list<std::string>>(line, ' ');
+    std::list<std::string> tmp_list = splitString<std::list<std::string> >(line, ' ');
     if (tmp_list.size() > 2 || tmp_list.size() == 0)
         return false;
     if (tmp_list.size() == 1) {
         const std::string& value = tmp_list.front();
         if (value.find("https://") != std::string::npos || value.find("http://") != std::string::npos)
             return true;
-        if (onlyDigits(value) && stoi(value) >= 300 && stoi(value) <= 599)
+        if (onlyDigits(value) && std::atoi(value.c_str()) >= 300 && atoi(value.c_str()) <= 599)
             return true;
         return false;
     }
     if (tmp_list.size() == 2) {
         const std::string&  status_code = tmp_list.front();
         const std::string&  path = tmp_list.back();
-        if (!onlyDigits(status_code) || stoi(status_code) < 300 || stoi(status_code) > 599)
+        if (!onlyDigits(status_code) || std::atoi(status_code.c_str()) < 300 || std::atoi(status_code.c_str()) > 599)
             return false;
         if (path.find("https://") == std::string::npos && path.find("http://") == std::string::npos)
             return false;
--- src/main.cpp
+++ src/main.cpp
@@ -7,3 +7,27 @@
     5.1 Check clients ()
     5.2 
 */
+
+#include "../include/ConfigParser.hpp"
+#include "../include/ConfigServer.hpp"
+#include "../include/ConfigLocation.hpp"
+
+int main(int ac, char **av)
+{
+    if (ac > 2)
+    {
+        std::cerr << "Usage: ./config_parser [config_file]" << std::endl;
+        return 1;
+    }
+    std::string file = "config/default.conf";
+    if (ac == 2)
+        file = av[1];
+    try {
+        ConfigParser parser(file);
+        parser.parseFile();
+    } catch (const char *e) {
+        std::cerr << "Error: " << e << std::endl;
+        return 1;
+    }
+    return 0;
+}
