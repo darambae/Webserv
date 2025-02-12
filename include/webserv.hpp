7ecf58f95fb5e8ee95df0e13de841a1456c74cbb
darambae <64197421+jazzy1101@users.noreply.github.com>
2025-02-12 15:01:01 +0100

    Fixed POLLHUP issue on mac OS

--- include/webserv.hpp
+++ include/webserv.hpp
@@ -67,6 +67,7 @@ struct Fd_data
 	Response*		response;
 	int				port;
 	std::string		ip;
+	bool			just_connected;
 };
 
 extern int MAX_CLIENT;//by default but max is defined by system parameters(bash = ulimit -n)
