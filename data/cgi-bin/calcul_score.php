aeae26a50f34635c29d84400a0ecefbf5440a166
kishoun <kellybrener66@gmail.com>
2025-02-26 22:38:11 +0100

    php work, have to send data and make it beautiful

--- data/cgi-bin/calcul_score.php
+++ data/cgi-bin/calcul_score.php
@@ -1,28 +1,58 @@
 <?php
+/*
+tout ce qui est en dehors des balise (<?php et ?>)est affiché dans la sortie standard
+$name, représente une variable
+$name = 1; ou $name="marc"; ou $name = 'marc';
+echo $name; pour l'afficher dans la stdout
+$note = 10; $note2 = 12;
+echo ($note + $note2)/2; écris 11 dans stdout
+$prénom = 'marc'; $nom = 'dupont';
+echo $prénom . $nom; pour des string, '.' au lieu de +
+'' : affiche tout sans interpréter
+"" : interprete les variables et les caractères spéciaux (\n)
 
+*/
 // Vérification que le formulaire a été soumis
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && $_SERVER['CONTENT_LENGTH'] > 0) {
     // Récupérer les données envoyées par le formulaire pour chaque joueur
-	$names = $_POST['name'] ?? [];
+	$content_length = (int) $_SERVER['CONTENT_LENGTH']; // Taille des données POST
+    $post_data = "";
+
+    // Lire STDIN en boucle jusqu'à recevoir toutes les données
+    $stdin = fopen("php://stdin", "r");
+    while (strlen($post_data) < $content_length) {
+        $post_data .= fread($stdin, 1024); // Lire 1 Ko à la fois
+    }
+    fclose($stdin);
+
+	// echo "data from parent : ";
+	// print_r($post_data);
+	parse_str($post_data, $parsed_postData);
+	$names = $parsed_postData['name'] ?? [];
 	$players = [
-		$_POST['player1'] ?? [],
-		$_POST['player2'] ?? [],
-		$_POST['player3'] ?? [],
-		$_POST['player4'] ?? [],
-		$_POST['player5'] ?? []
+		$parsed_postData['player1'] ?? [],
+		$parsed_postData['player2'] ?? [],
+		$parsed_postData['player3'] ?? [],
+		$parsed_postData['player4'] ?? [],
+		$parsed_postData['player5'] ?? []
 	];
 	$content = "<!DOCTYPE html>
 	<html>
 	<body>
 		<h2>Scores</h2>";
-    // Affichage des scores
+	// echo "data after parse_str : ";
+	// print_r($names);
+	// print_r($players);
+    //Affichage des scores
     for ($i = 0; $i < count($players); $i++) {
-		$content .= "$names[$i]";
         if (!empty($players[$i]) && !empty($names[$i])) {
+
             $content .= "<h2>" . htmlspecialchars($names[$i]) . "</h2><ul>";
-            foreach ($players[$i] as $score) {
-                $content .= "<li>" . htmlspecialchars($score) . "</li>";
+            $total = 0;
+			foreach ($players[$i] as $score) {
+				$total = $total + $score;
             }
+			$content .= "<h3>" . htmlspecialchars($total) . "</h3><ul>";
             $content .= "</ul>";
         }
     }
