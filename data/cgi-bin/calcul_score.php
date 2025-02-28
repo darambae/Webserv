582efe5d1667d6a9fb68f20a6e93d672aa369b02
kishoun <kellybrener66@gmail.com>
2025-02-28 14:46:27 +0100

    clean php

--- data/cgi-bin/calcul_score.php
+++ data/cgi-bin/calcul_score.php
@@ -12,6 +12,79 @@ echo $prénom . $nom; pour des string, '.' au lieu de +
 "" : interprete les variables et les caractères spéciaux (\n)
 
 */
+
+function extractDataFromBody($body) {
+	parse_str($body, $parsed_postData);
+		$names = $parsed_postData['name'] ?? [];
+		$players = [
+			$parsed_postData['player1'] ?? [],
+			$parsed_postData['player2'] ?? [],
+			$parsed_postData['player3'] ?? [],
+			$parsed_postData['player4'] ?? [],
+			$parsed_postData['player5'] ?? []
+		];
+		$data = [$names, $players];
+		return $data;
+}
+
+function fillReturnHead(&$content) {
+	$content = '<!DOCTYPE html>
+		<html lang="fr">
+		<head>
+			<meta charset="UTF-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<title>Scores</title>
+			<link rel="stylesheet" href="/cgi-bin/style.css">
+		</head>
+		<body>
+
+			<header>
+				<h1>Scores</h1>
+			</header>
+
+			<main>
+			<section>';
+}
+
+function fillReturnBody(&$content, array $players, array $names) {
+	$total_max = 0;
+	$winnerS[0] = "";
+	$winner = "";
+	for ($i = 0; $i < count($players); $i++) {
+		if (!empty($players[$i]) && !empty($names[$i])) {
+			$content .= '<div class="player">' . htmlspecialchars($names[$i]);
+			$total = 0;
+			foreach ($players[$i] as $score) {
+				$total = $total + $score;
+			}
+			$content .= '<span>' . htmlspecialchars($total) . '</span></div>';
+			if ($total >= $total_max) {
+				if ($total === $total_max && strlen($winnerS[0]) > 0) {
+					$winnerS[] = $names[$i];
+				}
+				else {
+					$total_max = $total;
+					$winner = $names[$i];
+					$winnerS[0] = $winner;
+				}
+			}
+		}
+	}
+	if (count($winnerS) > 1) {
+		$content .= '<div class="winner">🥇 ';
+		foreach ($winnerS as $name) {
+			$content .= ' / ' . htmlspecialchars(($name));
+		}
+		$content .= ' are the winners with a score of ' . '<span>' . htmlspecialchars($total_max) . '</span> <img src="/cgi-bin/trophee.png" alt="Trophée" width="40" class="trophy"></div>';
+	} else {
+		$content .= '<div class="winner">🥇 ' . htmlspecialchars(($winner)) . ' is the winner with a score of ' . '<span>' . htmlspecialchars($total_max) . '</span> <img src="/cgi-bin/trophee.png" alt="Trophée" width="40" class="trophy"></div>';
+	}
+	$content .= '</section>
+	</main>
+
+	</body>
+	</html>';
+}
 // Vérification que le formulaire a été soumis
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && $_SERVER['CONTENT_LENGTH'] > 0) {
     // Récupérer les données envoyées par le formulaire pour chaque joueur
@@ -27,83 +100,20 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && $_SERVER['CONTENT_LENGTH'] > 0) {
 
 	// echo "data from parent : ";
 	// print_r($post_data);
-	parse_str($post_data, $parsed_postData);
-	$names = $parsed_postData['name'] ?? [];
-	$players = [
-		$parsed_postData['player1'] ?? [],
-		$parsed_postData['player2'] ?? [],
-		$parsed_postData['player3'] ?? [],
-		$parsed_postData['player4'] ?? [],
-		$parsed_postData['player5'] ?? []
-	];
-	$content = '<!DOCTYPE html>
-	<html lang="fr">
-	<head>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>Scores</title>
-		<link rel="stylesheet" href="/cgi-bin/style.css">
-	</head>
-	<body>
-
-		<header>
-			<h1>Scores</h1>
-		</header>
-
-		<main>
-		<section>';
-		$total_max = 0;
-		$winnerS[0] = "";
-		$winner = "";
-		for ($i = 0; $i < count($players); $i++) {
-			if (!empty($players[$i]) && !empty($names[$i])) {
-				$content .= '<div class="player">' . htmlspecialchars($names[$i]);
-				$total = 0;
-				foreach ($players[$i] as $score) {
-					$total = $total + $score;
-				}
-				$content .= '<span>' . htmlspecialchars($total) . '</span></div>';
-				if ($total >= $total_max) {
-					if ($total === $total_max && strlen($winnerS[0]) > 0) {
-						$winnerS[] = $names[$i];
-					}
-					else {
-						$total_max = $total;
-						$winner = $names[$i];
-						$winnerS[0] = $winner;
-					}
-				}
-			}
-		}
-		if (count($winnerS) > 1) {
-			$content .= '<div class="winner">🥇 ';
-			foreach ($winnerS as $name) {
-				$content .= ' / ' . htmlspecialchars(($name));
-			}
-			$content .= ' are the winners with a score of ' . '<span>' . htmlspecialchars($total_max) . '</span> <img src="/cgi-bin/trophee.png" alt="Trophée" width="40" class="trophy"></div>';
-		} else {
-			$content .= '<div class="winner">🥇 ' . htmlspecialchars(($winner)) . ' is the winner with a score of ' . '<span>' . htmlspecialchars($total_max) . '</span> <img src="/cgi-bin/trophee.png" alt="Trophée" width="40" class="trophy"></div>';
-		}
-			$content .= '</section>
-		</main>
-
-	</body>
-	</html>';
-
-	// echo "data after parse_str : ";
-	// print_r($names);
-	// print_r($players);
-    //Affichage des scores
-
-	$length = strlen($content);
-	echo "Content-Type: text/html\r\n";
-	echo "Content-Length: $length\r\n\r\n";
-	echo $content;
+	if (strlen($post_data) > 0) {
+		$data = extractDataFromBody($post_data);
+		$content = "";
+		fillReturnHead($content);
+		fillReturnBody($content, $data[1], $data[0]);
+		$length = strlen($content);
+		echo "Content-Type: text/html\r\n";
+		echo "Content-Length: $length\r\n\r\n";
+		echo $content;
+	}
 } else {
     // Si l'utilisateur tente d'accéder directement à la page sans soumettre le formulaire
-    header("HTTP/1.1 405 Method Not Allowed");
+    	header("HTTP/1.1 405 Method Not Allowed");
     // echo "Méthode non autorisée. Veuillez soumettre un formulaire.";
-    exit;
+    	exit;
 }
 ?>
-
