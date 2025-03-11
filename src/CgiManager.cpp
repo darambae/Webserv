d07d71bd5a353ca19bbf894f52bc0ba9de9effc2
Daram Bae <baedaram@gmail.com>
2025-03-11 14:48:10 +0000

    Working on linux but not on macOS

--- src/CgiManager.cpp
+++ src/CgiManager.cpp
@@ -2,10 +2,9 @@
 
 CgiManager::CgiManager(CGI_env*	cgi_env, Request* request, Response* response) :
 	 _cgi_env(cgi_env), _request(request), _response(response), _headerDoneReading(false) {
-		_cgiResponseStatus = 200;
-		_children_done = false;
-	_children_status = -2;
 	_cgiResponseStatus = 200;
+	_children_done = false;
+	_children_status = -2;
 	std::ifstream   python_path("python3_path.txt");
 	if (!python_path.is_open())
 		LOG_ERROR("The file that has Python3 path can't be opened", 1);
