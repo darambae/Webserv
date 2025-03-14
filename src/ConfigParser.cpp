dd30c525ed5bd901758f2282823b4db372a65dc6
Daram Bae <baedaram@gmail.com>
2025-03-14 10:00:19 +0000

    return data structure changed from vector to map

--- src/ConfigParser.cpp
+++ src/ConfigParser.cpp
@@ -205,7 +205,6 @@ void    ConfigParser::validReturn(const std::string& line) {
         const std::string&  path = tmp_vector.back();
         if (!onlyDigits(status_code) || std::atoi(status_code.c_str()) < 300 || std::atoi(status_code.c_str()) > 599)
             THROW("Invalid return status code");
-        validRoot(path);
         return;
     }   
     THROW("Invalid return");
